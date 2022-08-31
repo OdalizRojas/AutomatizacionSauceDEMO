bcaf98cdea2ea990dac5a0255aac2b8f7d7fa521
“Odaliz <odaliz.rojasroldan@aiesec.net>
2022-08-31 00:12:14 -0400

    Los test en la pagina del carrito

--- sauce-labs-automation/src/test/java/CartTest.java
+++ sauce-labs-automation/src/test/java/CartTest.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class CartTest {
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+import pages.Homepage;
+import pages.LoginPage;
+import utilities.DriverManager;
+import pages.CartPage;
+
+public class CartTest extends BaseTest{
+
+    @Test
+    public void VerifyNumberOfItemsInDetailOfProduct(){
+        LoginPage loginPage = new LoginPage(DriverManager.getDriver().driver);
+        loginPage.setUserNameTextBox("standard_user");
+        loginPage.setPasswordTextBox("secret_sauce");
+        loginPage.clickOnLoginButton();
+        Homepage homepage= new Homepage(DriverManager.getDriver().driver);
+        homepage.clickOnAddSauceLabsBackPackToCartButton();
+        CartPage cartPage = new CartPage(DriverManager.getDriver().driver);
+        Assert.assertTrue(cartPage.priceProductDisplayed());
+    }
+
+    @Test @Ignore
+    public void VerifyNumberOfItemsForProduct(){
+        LoginPage loginPage = new LoginPage(DriverManager.getDriver().driver);
+        loginPage.setUserNameTextBox("standard_user");
+        loginPage.setPasswordTextBox("secret_sauce");
+        loginPage.clickOnLoginButton();
+        Homepage homepage= new Homepage(DriverManager.getDriver().driver);
+        homepage.clickOnAddSauceLabsBackPackToCartButton();
+        CartPage cartPage = new CartPage(DriverManager.getDriver().driver);
+
+    }
+
+
 }
