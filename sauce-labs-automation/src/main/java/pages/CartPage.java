3a9dd0f143fdfbd1cf68bba49ba51beb6e6cb2be
“Odaliz <odaliz.rojasroldan@aiesec.net>
2022-08-30 23:59:49 -0400

    Actualizando Cart Page con los metodos para el test del examen

--- sauce-labs-automation/src/main/java/pages/CartPage.java
+++ sauce-labs-automation/src/main/java/pages/CartPage.java
@@ -4,6 +4,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class CartPage {
 
@@ -11,6 +15,27 @@ public class CartPage {
 
     @FindBy(className = "inventory_item_price")
     WebElement priceProduct;
+
+    @FindBy(xpath = "//*[@id=\"item_5_title_link\"]/div")
+    WebElement textJacket;
+
+    @FindBy(xpath = "//*[@id=\"item_0_title_link\"]/div")
+    WebElement textBike;
+
+    @FindBy(xpath = "//*[@id=\"cart_contents_container\"]/div/div[1]/div[3]/div[2]/div[2]/div")
+    WebElement bikePrice;
+
+    @FindBy(xpath = "//*[@id=\"cart_contents_container\"]/div/div[1]/div[4]/div[2]/div[2]/div")
+    WebElement jacketPrice;
+
+    @FindBy(className = "shopping_cart_badge")
+    WebElement cartIconNumber;
+
+    @FindBy(id = "remove-sauce-labs-bike-light")
+    WebElement removeButtonBike;
+
+    @FindBy(id = "remove-sauce-labs-fleece-jacket")
+    WebElement removeButtonJacket;
 
     public CartPage(WebDriver driver){
         this.driver=driver;
@@ -22,4 +47,39 @@ public class CartPage {
         return priceProductIsDisplayed;
     }
 
+    public boolean jacketTextIsDisplayed(){
+        boolean jacketLogoIsDisplayed = textJacket.isDisplayed();
+        return jacketLogoIsDisplayed;
+    }
+
+    public boolean bikeTextIsDisplayed(){
+        boolean bikeLogoIsDisplayed = textBike.isDisplayed();
+        return bikeLogoIsDisplayed;
+    }
+    public Double getBikePrice(){
+        Double bikePrice = Double.parseDouble(textBike.getText());
+        return bikePrice;
+    }
+    public Double getJacketPrice(){
+        System.out.println(jacketPrice.getText());
+        Double jacket = Double.parseDouble(jacketPrice.getText());
+        return jacket;
+    }
+
+    public String getCartNumberText(){
+        String cartNumberText = cartIconNumber.getText();
+        return cartNumberText;
+    }
+
+    public void clickOnRemoveBikeToCart(){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.elementToBeClickable(removeButtonBike));
+        removeButtonBike.click();
+    }
+
+    public void clickOnRemoveJacketToCart(){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.elementToBeClickable(removeButtonJacket));
+        removeButtonJacket.click();
+    }
 }
--- sauce-labs-automation/src/main/java/pages/Homepage.java
+++ sauce-labs-automation/src/main/java/pages/Homepage.java
@@ -1,8 +1,17 @@
 package pages;
+import com.google.common.collect.Ordering;
+import io.opentelemetry.semconv.resource.attributes.ResourceAttributes;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Homepage {
     WebDriver driver;
@@ -19,6 +28,29 @@ public class Homepage {
     @FindBy(id = "remove-sauce-labs-backpack")
     WebElement sauceLabsBackPackRemoveToCartButton;
 
+    @FindBy(className = "product_sort_container")
+    WebElement sauceLabSortDropDow;
+
+    @FindBy(className = "inventory_item_name")
+    List<WebElement> products;
+
+    @FindBy(className = "product_sort_container")
+    WebElement productFilterDropDown;
+
+    @FindBy(className = "inventory_item_price")
+    List<WebElement> itemPricesLebel;
+
+    // ------- Boton "ADD TO CART" de Sauce Labs Bike Light
+
+    @FindBy(id = "add-to-cart-sauce-labs-bike-light")
+    WebElement productBikeLight;
+
+    // ------- Boton "ADD TO CART" de Sauce Labs Fleece Jacket
+
+    @FindBy(id = "add-to-cart-sauce-labs-fleece-jacket")
+    WebElement productJacket;
+
+
     public Homepage(WebDriver driver){
         this.driver=driver;
         PageFactory.initElements(driver, this);
@@ -30,8 +62,8 @@ public class Homepage {
     }
 
     public String getCartIconText(){
-        String cartText = cartIcon.getText();
-        return cartText;
+        String cartatext = cartIcon.getText();
+        return cartatext;
     }
 
     public void clickOnAddSauceLabsBackPackToCartButton(){
@@ -40,5 +72,47 @@ public class Homepage {
 
     public void clickOnRemoveSauceLabsBackPackToCartButton(){
         sauceLabsBackPackRemoveToCartButton.click();
+    }
+
+    public void clickOnAddBikeLightToCartButton(){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.elementToBeClickable(productBikeLight));
+        productBikeLight.click();
+    }
+
+    public void clickOnAddJacketToCartButton(){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.elementToBeClickable(productJacket));
+        productJacket.click();
+    }
+
+    public void selectObject(String element){
+        Select select = new Select(sauceLabSortDropDow);
+        select.selectByVisibleText(element);
+    }
+
+    public List<String> getAllItems(){
+        List<String> items = new ArrayList<>();
+        for(WebElement item: products){
+            String itemText = item.getText();
+            StringBuilder sb = new StringBuilder(itemText);
+            sb.deleteCharAt(0);
+            items.add(sb.toString());
+        }
+        return items;
+    }
+    public void selectProductFilter(String element){
+        Select selectObject = new Select(productFilterDropDown);
+        selectObject.selectByVisibleText(element);
+    }
+    public List<Double> getAllItemPrices(){
+        List<Double> prices = new ArrayList<>();
+        for(WebElement itemPrice: itemPricesLebel){
+            String itemPriceText = itemPrice.getText();
+            StringBuilder sb = new StringBuilder(itemPriceText);
+            sb.deleteCharAt(0);
+            prices.add(Double.parseDouble(sb.toString()));
+        }
+        return prices;
     }
 }
